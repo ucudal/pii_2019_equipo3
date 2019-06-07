6f52c0d8f04226d70a7f74d9ec55d57475c0c8d0
JoaquinAlcalde <48680966+JoaquinAlcalde@users.noreply.github.com>
2019-06-06 23:33:07 -0300

    Add files via upload

---
+++ Models/Person.cs
@@ -0,0 +1,13 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesIgnis.Models
+{
+    public abstract class Person
+    {
+       public int ID { set; get; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Mail { get; set; }
+    }
+}
